f0fbbeaa8cc9285f49ae087cd99760a34d298dbf
Nikhil <nmohanty1@hawk.iit.edu>
2019-03-02 16:50:03 -0600

    Updated Css, Changed line height, added submit button

--- project_1/site.js
+++ project_1/site.js
@@ -1,3 +1,11 @@
+(function(){
+  if(!('querySelector' in document && 'addEventListener' in document)){
+
+    return;
+  }
+}
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
   console.log('DOM Content Loaded');
